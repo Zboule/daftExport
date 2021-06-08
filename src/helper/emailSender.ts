dd8e6688b69b54fa067775a0c3eb1bad91fa832b
jordane <jordane.cure@gmail.com>
2021-06-08 20:38:57 +0100

    Support multi zones

--- src/helper/emailSender.ts
+++ src/helper/emailSender.ts
@@ -1,28 +1,40 @@
 import { link } from "fs";
 
-
 var aws = require("aws-sdk");
-var ses = new aws.SES({ region: 'eu-west-1'  });
+var ses = new aws.SES({ region: "eu-west-1" });
 
-
-export async function sendEmail (listing: string,links: string[]) {
-
-    var params = {
-        Destination: {
-          ToAddresses: ['jordane.cure@gmail.com','alexanyan.n@gmail.com'],
+export async function sendEmail(listing: string[], links: string[]) {
+  var params = {
+    Destination: {
+      ToAddresses: ["jordane.cure@gmail.com", "alexanyan.n@gmail.com"],
+    },
+    Message: {
+      Body: {
+        Html: {
+          Data: `<div>
+                <h2>Searchs</h2>
+                ${listing
+                  .map(
+                    (listing, i) =>
+                      `<p><a href="${listing}">Search ${i + 1}</a></p>`
+                  )
+                  .join("")}
+                <h2>New content</h2>
+                ${links
+                  .map(
+                    (link) =>
+                      `<p><a href="https://www.daft.ie${link}">${link}</a></p>`
+                  )
+                  .join("")}
+            </div>`,
         },
-        Message: {
-          Body: {
-            Html: { Data: `<div>
-                <h3>New content in <a href="${listing}">this</a> research</h3>
-                ${links.map((link) => `<p><a href="https://www.daft.ie${link}">${link}</a></p>`).join('')}
-            </div>` },
-          },
-    
-          Subject: { Data: "New daft properties" },
-        },
-        Source: "jordane.cure@gmail.com",
-      };
-     
-    await ses.sendEmail(params).promise()
+      },
+
+      Subject: { Data: "New daft properties" },
+    },
+    Source: "jordane.cure@gmail.com",
+  };
+
+  console.log('send email')
+  await ses.sendEmail(params).promise();
 }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
-import { extractLinks, getPageContent } from './helper/extract'
-import { getSavedContent, uploadContent } from './helper/storage'
-import { sendEmail } from './helper/emailSender'
+import { extractLinks, getPageContent } from "./helper/extract";
+import { getSavedContent, uploadContent } from "./helper/storage";
+import { sendEmail } from "./helper/emailSender";
 
-
-
-
-let search = `https://www.daft.ie/property-for-rent/mapArea?geoSearchType=BBOX
+let searchs = [
+  `https://www.daft.ie/property-for-rent/mapArea?geoSearchType=BBOX
                 &top=53.3652
                 &left=-6.2695
                 &right=-6.2538
@@ -15,28 +13,57 @@ let search = `https://www.daft.ie/property-for-rent/mapArea?geoSearchType=BBOX
                 &rentalPrice_from=2000
                 &rentalPrice_to=3000
                 &sort=publishDateDesc
-                &numBeds_to=2`
+                &numBeds_to=2`,
+  `https://www.daft.ie/property-for-rent/mapArea?geoSearchType=BBOX
+                &top=53.34629133972456
+                &left=-6.269738042692097
+                &right=-6.259335595173894
+                &bottom=53.33856932792074
+                &mapView=true
+                &showMap=false
+                &rentalPrice_from=2000
+                &rentalPrice_to=3000
+                &sort=publishDateDesc
+                &numBeds_to=2`,
+  `https://www.daft.ie/property-for-rent/mapArea?geoSearchType=BBOX
+                &top=53.33213698795032
+                &left=-6.273544399312044
+                &right=-6.2618233673758255
+                &bottom=53.32778151417952
+                &mapView=true
+                &showMap=false
+                &rentalPrice_from=2000
+                &rentalPrice_to=3000
+                &sort=publishDateDesc
+                &numBeds_to=2`,
+].map((url) =>
+  url
+    .replace(/(\r\n|\n|\r)/gm, "")
+    .replace(/\t/g, "")
+    .replace(/\s/g, "")
+);
 
 export const handler = async (event: any = {}): Promise<any> => {
-    let savedContent = await getSavedContent()
-    let pageContent = await getPageContent(search).then(extractLinks)
+  let savedContent = await getSavedContent();
+  let newAddedContent: string[] = [];
 
-    let newAddedContent: string [] = []
-    pageContent.forEach(url => {
-        if (savedContent[url] === undefined) {
-            newAddedContent.push(url)
-            console.log('New content:', url)
-            savedContent[url] = new Date().toISOString().split('T')[0]
-        }
+  for (let search of searchs) {
+    let pageContent = await getPageContent(search).then(extractLinks);
+    pageContent.forEach((url) => {
+      if (savedContent[url] === undefined) {
+        newAddedContent.push(url);
+        console.log("New content:", url);
+        savedContent[url] = new Date().toISOString().split("T")[0];
+      }
     });
+  }
 
-    await uploadContent(savedContent)
+  await uploadContent(savedContent);
 
-    if (newAddedContent.length > 0){
-        console.log('Send an email')
-        await sendEmail(search,newAddedContent)
-    }
-  
-    return newAddedContent
-}
+  if (newAddedContent.length > 0 ) {
+    console.log("Send an email");
+    await sendEmail(searchs, newAddedContent);
+  }
 
+  return newAddedContent;
+};
